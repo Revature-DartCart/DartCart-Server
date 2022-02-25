f22a8ba76031f05823a733b15274b39fad667ba4
Joshua-Lee-Harms <98427613+Joshua-Lee-Harms@users.noreply.github.com>
2022-02-25 09:01:22 -0600

    initial mocking in Category service tests

--- src/test/java/com/revature/services/CategoryServicesTests.java
+++ src/test/java/com/revature/services/CategoryServicesTests.java
@@ -1,4 +1,23 @@
 package com.revature.services;
 
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@SpringBootTest(classes = {})
 public class CategoryServicesTests {
+
+    @Autowired
+    CategoryService cs;
+
+    @MockBean
+    private CategoryRepo cp;
+
+
+    
+
+
+
 }
