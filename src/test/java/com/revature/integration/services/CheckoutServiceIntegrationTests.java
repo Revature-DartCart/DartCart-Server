1ddf7998c5b6a34d76cab406e5a832f239496ac5
RichardSimpson235 <57775052+RichardSimpson235@users.noreply.github.com>
2022-03-05 20:31:11 -0500

    Add @DiritiesContext since these tests manipulate the database

--- src/test/java/com/revature/integration/services/CheckoutServiceIntegrationTests.java
+++ src/test/java/com/revature/integration/services/CheckoutServiceIntegrationTests.java
@@ -8,11 +8,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = DartCartApplication.class)
 public class CheckoutServiceIntegrationTests {
 
