dc6220e217247abe85bc0be7ba4b5a0f09820df2
RichardSimpson235 <57775052+RichardSimpson235@users.noreply.github.com>
2022-03-05 20:27:28 -0500

    Add @JsonBackReference to user field
    If this entity is a child of one being serialized we don't need to send its user data.

--- src/main/java/com/revature/models/Seller.java
+++ src/main/java/com/revature/models/Seller.java
@@ -1,5 +1,6 @@
 package com.revature.models;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -28,6 +29,7 @@ public class Seller {
 
     @OneToOne
     @JoinColumn(name="user_id")
+    @JsonBackReference
     private User user;
 
 }
