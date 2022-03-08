b40ca846da2c26d4c49743614820e2e32e81fef4
David Melanson Jr <david427@live.com>
2022-03-08 17:29:31 -0500

    Update SellerController.java

--- src/main/java/com/revature/controllers/SellerController.java
+++ src/main/java/com/revature/controllers/SellerController.java
@@ -11,5 +11,16 @@ public class SellerController {
 
     @Autowired
     SellerService sellerService;
+    
+    // There's no need for an addSeller() method. It's handled by the /signup endpoint in ShopController,
+    // since by signing up as a Seller, you also create your first Shop automatically.
+
+    @GetMapping("/sellers/{id}")
+    public ResponseEntity<Seller> getSellerById(@PathVariable("id") String id) {
+        Optional<Seller> seller = sellerService.getSellerByUserId(Integer.parseInt(id));
+        return seller
+            .map(value -> new ResponseEntity<>(value, HttpStatus.OK))
+            .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
 
 }
