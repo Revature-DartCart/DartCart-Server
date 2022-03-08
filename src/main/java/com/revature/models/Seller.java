71f75b95e97c0b106261f75e7ae43d584ddb51bb
RichardSimpson235 <57775052+RichardSimpson235@users.noreply.github.com>
2022-03-08 09:17:34 -0500

    Add NotNull annotation imports

--- src/main/java/com/revature/models/Seller.java
+++ src/main/java/com/revature/models/Seller.java
@@ -1,6 +1,7 @@
 package com.revature.models;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -37,6 +38,4 @@ public class Seller {
     @JoinColumn(name="user_id")
     @JsonBackReference
     private User user;
-
-
 }
--- src/main/java/com/revature/models/User.java
+++ src/main/java/com/revature/models/User.java
@@ -1,5 +1,6 @@
 package com.revature.models;
 
+import com.sun.istack.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
