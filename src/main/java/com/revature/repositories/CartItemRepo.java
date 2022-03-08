318e28dd5c6b6e63eb7fe01bf9223978ba0b822c
RichardSimpson235 <57775052+RichardSimpson235@users.noreply.github.com>
2022-03-08 16:18:05 -0500

    Change field name to user_id to match database

--- src/main/java/com/revature/repositories/CartItemRepo.java
+++ src/main/java/com/revature/repositories/CartItemRepo.java
@@ -10,12 +10,12 @@ import java.util.List;
 @Repository
 public interface CartItemRepo extends CrudRepository<CartItem, Integer> {
     
-    @Query(value = "SELECT * FROM Cart_Items WHERE customer_id=?", nativeQuery = true)
+    @Query(value = "SELECT * FROM Cart_Items WHERE user_id=?", nativeQuery = true)
     List<CartItem> getAllCartItem(int id);
 
-    @Query(value = "SELECT * FROM Cart_Items WHERE customer_id=?", nativeQuery = true)
+    @Query(value = "SELECT * FROM Cart_Items WHERE user_id=?", nativeQuery = true)
     CartItem getCurrentCart(int userId);
 
-    @Query(value = "SELECT * FROM Cart_Items WHERE SHOP_PRODUCT_SHOP_PRODUCT_ID=? and customer_id=? limit 1", nativeQuery = true)
+    @Query(value = "SELECT * FROM Cart_Items WHERE SHOP_PRODUCT_SHOP_PRODUCT_ID=? and user_id=? limit 1", nativeQuery = true)
     CartItem getByShopProductId(int ShopProductId, int userId);
 }
