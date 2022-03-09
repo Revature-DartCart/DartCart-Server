d2e73b20be5db464537c54216e3efc4d8ae0c75e
David Melanson Jr <david427@live.com>
2022-03-09 13:17:48 -0500

    Remove duplicate Seller methods

--- src/main/java/com/revature/controllers/SellerController.java
+++ src/main/java/com/revature/controllers/SellerController.java
@@ -27,15 +27,4 @@ public class SellerController {
             .map(value -> new ResponseEntity<>(value, HttpStatus.OK))
             .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
-
-    // There's no need for an addSeller() method. It's handled by the /signup endpoint in ShopController,
-    // since by signing up as a Seller, you also create your first Shop automatically.
-
-    @GetMapping("/sellers/users/{id}")
-    public ResponseEntity<Seller> getSellerById(@PathVariable("id") String id) {
-        Optional<Seller> seller = sellerService.getSellerByUserId(Integer.parseInt(id));
-        return seller
-            .map(value -> new ResponseEntity<>(value, HttpStatus.OK))
-            .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
-    }
 }
